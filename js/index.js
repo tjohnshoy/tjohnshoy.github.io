398a1441aec35a21c6fbe105f1a878a799af1d25
tjohnshoy <65180031+tjohnshoy@users.noreply.github.com>
2020-10-20 18:17:50 -0500

    jquery

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log("Your index.js file is loading correctly!");
+
+$("#myBtn").on("click", function(){
+    $(window).scrollTop(0);
+});
+$("#myBtn").on("click", function(){
+    console.log("You clicked on #myID");
+})
