3856bb7c293042b60f3c9a518badaf15df3de61a
tjohnshoy <65180031+tjohnshoy@users.noreply.github.com>
2021-11-05 16:31:26 -0500

    11.5.21

--- js/index.js
+++ js/index.js
@@ -1,9 +1,29 @@
-console.log("Your index.js file is loading correctly!");
+// console.log("Your index.js file is loading correctly!");
 
-$("#myBtn").on("click", function(){
-    $(window).scrollTop(0);
-});
-$("#myBtn").on("click", function(){
-    console.log("You clicked on #myID");
-})
+// $("#myBtn").on("click", function(){
+//     $(window).scrollTop(0);
+// });
+// $("#myBtn").on("click", function(){
+//     console.log("You clicked on #myID");
+// })
 
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
